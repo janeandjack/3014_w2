031941b5b1f1ab23ce2743f94610f1317c14ac61
Li <c_li70655@tss-hr420-dm09.fc.ca>
2019-01-30 13:16:50 -0500

    6

---
+++ js/components/UsersComponent.js
@@ -0,0 +1,19 @@
+export default {
+    template:`
+    <div id="login">
+    <h1>Log In </h1>
+    </div>
+    `,
+    
+    
+    
+        data(){
+            return {
+               message: "hello",
+               userlist: []
+                }
+            }
+      
+   
+            }
+  
